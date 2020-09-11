2d471981bc63931cb16fa4e021b26797bcd21e31
Meline Abrahamyan <ab-mel@yandex.ru>
2020-09-11 17:26:48 +0400

    modified conv_test file

--- conv_test.py
+++ conv_test.py
@@ -1,50 +1,50 @@
 # conv_test.py
 
-from matrices import Matrix
-from convolution import Convolution
-import os
+from convolution import Convolution, Matrix
 import time
 
 
 matrix = Matrix()
 
-# TEST 1
-"""input matrix (1x32x32) , filter (1x5x5), output (1x28x28)"""
+def test():
 
-# geting input matrix, filter_matrix
-input_matrix = matrix.read_from_file('input2.txt')
-filter_matrix = matrix.read_from_file('filter1.txt')
-bias = 1
-# calling convolution3D
-convolution = Convolution(input_matrix, filter_matrix, bias)
-conv = convolution.convolution_3d()
+    # TEST 1
+    """input matrix (1x32x32) , filter (1x5x5), output (1x28x28)"""
+
+    # geting input matrix, filter_matrix
+    input_matrix = matrix.read_from_file('input2.txt')
+    filter_matrix = matrix.read_from_file('filter1.txt')
+    bias = 1
+    # calling convolution3D
+    convolution = Convolution(input_matrix, filter_matrix, bias)
+    conv = convolution.convolution_3d()
    
-# writing to file
+    # writing to file
 
-result = matrix.write_to_file('output.json', conv)
+    result = matrix.write_to_file('output.json', conv)
     
-print('Test 1 Done!')
+    print('Test 1 Done!')
 
-time.sleep(2)
+    time.sleep(1)
 
-# TEST 2
-""" input matrix (2x5x5), filter (2x3x3), output (1x3x3)"""
+    # TEST 2
+    """ input matrix (2x5x5), filter (2x3x3), output (1x3x3)"""
 
-# geting input matrix, filter_matrix
-input_matrix = matrix.read_from_file('input.txt')
-filter_matrix = matrix.read_from_file('filter.txt')
-bias = 0
-# calling convolution3D
-cconvolution = Convolution(input_matrix, filter_matrix, bias)
-onv = convolution.convolution_3d()
+    # geting input matrix, filter_matrix
+    input_matrix = matrix.read_from_file('input.txt')
+    filter_matrix = matrix.read_from_file('filter.txt')
+    bias = 0
+    # calling convolution3D
+    convolution = Convolution(input_matrix, filter_matrix, bias) 
+    conv = convolution.convolution_3d()
    
-# writing to file
+    # writing to file
 
-result = matrix.write_to_file('output_1.json', conv)
+    result = matrix.write_to_file('output_1.json', conv)
     
-print('Test 2 Done!')
+    print('Test 2 Done!')
 
 
-
-
-                    
+if __name__ == "__main__":
+    pass                
+               
